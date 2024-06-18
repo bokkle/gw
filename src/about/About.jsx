2dd7f7a1a287b0a6e491c7caaf551895ef4dfaa1
bokkle <mitch.r_@hotmail.com>
2024-06-17 23:38:55 -0400

    .

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,6 +1,7 @@
 import AboutButtons from './AboutButtons';
 import AboutHeading from './AboutHeading';
 import ContactAddress from './ContactAddress';
+import { Slide } from 'react-awesome-reveal';
 
 const About = () => {
   return (
@@ -9,9 +10,11 @@ const About = () => {
       className="mt-12 flex justify-center p-2 text-slate-100 md:mt-24"
     >
       <div className="flex max-w-7xl flex-col gap-20">
-        <AboutHeading />
-        <ContactAddress />
-        <AboutButtons />
+        <Slide direction="up" duration={1000} fraction={0.2} triggerOnce>
+          <AboutHeading />
+          <ContactAddress />
+          <AboutButtons />
+        </Slide>
       </div>
     </div>
   );
--- src/about/AboutHeading.jsx
+++ src/about/AboutHeading.jsx
@@ -1,7 +1,7 @@
 const AboutHeading = () => {
   return (
     <h1 className="text-center text-3xl capitalize md:text-5xl lg:text-7xl">
-      experience the limitless applications of a goth waifu companion
+     the time of the 3D whore is over... Make waifu&apos;s great again.
     </h1>
   );
 };
